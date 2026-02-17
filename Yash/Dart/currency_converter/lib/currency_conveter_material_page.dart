b95680dfb0884a5748e76b0b26ef6876ca8b28f0
Stranger4uu <yashsaini4824@gmail.com>
2026-02-17 21:23:34 +0530

    did some more changes in my currency converter app and i am going to fully complete it by today night only , for now i am moving to py

--- Yash/Dart/currency_converter/lib/currency_conveter_material_page.dart
+++ Yash/Dart/currency_converter/lib/currency_conveter_material_page.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 
-class MaterialHomePage extends StatelessWidget{
+class MaterialHomePage extends StatelessWidget {
   const MaterialHomePage({super.key});
   @override
   Widget build(BuildContext context) {
-      return const Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                Text(
-                  "Yash", // Replace "Yash" with a variable or parameter if needed
-                  style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-            ],
-          )
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Please enter the amount in USD',
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
-
 }
